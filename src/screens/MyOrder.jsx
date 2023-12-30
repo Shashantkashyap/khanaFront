cce779a0442fd59a9f13ddfa0bd690247c6ea29a
Shashant Kashyap <shashant.kashyap999@gmail.com>
2023-12-30 12:23:33 +0530

    Update MyOrder.jsx

--- src/screens/MyOrder.jsx
+++ src/screens/MyOrder.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Footer from '../components/Footer';
+import Footer from '../Components/Footer';
 import Navbar from '../Components/Navbar';
 
 export default function MyOrder() {
